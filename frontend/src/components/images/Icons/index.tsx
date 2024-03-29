668d181b1307ab6d4c63e40d82317b66f34ae4b1
sangheun969 <sang969@naver.com>
2024-03-30 00:28:22 +0900

    feat: Main Intro

---
+++ frontend/src/components/images/Icons/index.tsx
@@ -0,0 +1,17 @@
+import iconsGithub2 from "./iconsGithub2.png";
+import itemIcon from "./itemIcon.png";
+import menuBar2 from "./menuBar2.png";
+import messageIcon from "./messageIcon.png";
+import metamaskLogo1 from "./metamask_logo.png";
+import metamaskLogo2 from "./metamask_logo_2.png";
+import personIcon from "./personIcon.png";
+
+export {
+  iconsGithub2,
+  itemIcon,
+  menuBar2,
+  messageIcon,
+  metamaskLogo1,
+  metamaskLogo2,
+  personIcon,
+};
---
+++ frontend/src/components/molecules/MainBanner.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { orkBanner, coupleBanner2 } from "../images/Banners";
+
+const MainBanner = () => {
+  return (
+    <div className="flex items-center justify-center bg-center bg-cover ">
+      <img src={orkBanner} className="w-full h-screen mobile:h-[300px]" />
+      <div className="absolute h-screen inset-0 bg-black opacity-60 mobile:h-[300px]"></div>
+      <div className="absolute z-10 text-white text-center">
+        <p className="text-4xl">Test</p>
+        <p className="italic p-[20px] mt-[50px]">"Banner test"</p>
+      </div>
+    </div>
+  );
+};
+
+export default MainBanner;
