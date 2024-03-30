88dcfec4205f484341b5252287d7a2b9dd8286be
sangheun969 <sang969@naver.com>
2024-03-30 10:30:58 +0900

    set NextJs

--- frontend/src/components/molecules/ViewBox.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ViewBox = () => {
-  return <div></div>;
-};
-
-export default ViewBox;
--- frontend/src/images/Banners/index.tsx
+++
@@ -1,6 +0,0 @@
-import coupleBanner from "./coupleBanner.jpeg";
-import coupleBanner2_1 from "./coupleBanner2_1.jpeg";
-import coupleBanner2 from "./coupleBanner2.png";
-import orkBanner from "./orkBanner2.png";
-
-export { coupleBanner, coupleBanner2_1, coupleBanner2, orkBanner };
