38feb13a0951d141a417f5925754264d57e97f7b
chamdom.eth <wjdtmdry88@gmail.com>
2024-03-30 22:19:56 +0900

    Revert "fix : suscribe"

--- front/packages/hardhat/contracts/EscrowProxy.sol
+++ front/packages/hardhat/contracts/EscrowProxy.sol
@@ -1,9 +1,9 @@
-//SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
 import "./Escrow.sol";
 
-contract EscrowProxy {
+contract NaegiftEscrowProxy {
     address public latestEscrow;
     address public escrowImplementation;
 
--- front/packages/hardhat/hardhat.config.ts
+++ front/packages/hardhat/hardhat.config.ts
@@ -21,7 +21,7 @@ const etherscanApiKey = process.env.ETHERSCAN_API_KEY || "DNXJA8RX2Q3VZ4URQIWP7Z
 
 const config: HardhatUserConfig = {
   solidity: {
-    version: "0.8.20",
+    version: "0.8.17",
     settings: {
       optimizer: {
         enabled: true,
@@ -30,7 +30,7 @@ const config: HardhatUserConfig = {
       },
     },
   },
-  defaultNetwork: "sepolia",
+  defaultNetwork: "localhost",
   namedAccounts: {
     deployer: {
       // By default, it will take the first Hardhat account as the deployer
